f2dfb7141007fc87871a192d5df1485b1a947a6a
Justus marwa <83753891+justus-ai@users.noreply.github.com>
2026-04-05 10:11:11 +0000

    edit footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Mail, Phone, MapPin, Clock3, Facebook, Instagram, Linkedin } from 'lucide-react';
+import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
 import './Footer.css';
 
 const Footer = () => {
@@ -9,35 +9,25 @@ const Footer = () => {
 
       <div className='footer-inner'>
         <div>
-          <h3>Amigo Rentals</h3>
+          <h3>Amigon Rentals</h3>
           <p>Find your next home with confidence and comfort.</p>
-        </div>
+          <p>Open 24 Hours</p>
 
-        <div>
-          <h4>Quick Links</h4>
-          <ul className='footer-links'>
-            <li><a href='#'>Properties</a></li>
-            <li><a href='#'>Locations</a></li>
-            <li><a href='#'>Contact</a></li>
-          </ul>
-        </div>
-
-        <div>
-          <h4>Contact</h4>
-          <p><Phone size={16} /> +254 790 443 776</p>
-          <p><Mail size={16} /> info@amigorentals.com</p>
-          <p><MapPin size={16} /> Nairobi, Kenya</p>
-          <p><Clock3 size={16} /> Open 24 Hours</p>
-          <div className='socials'>
-            <a href='#' aria-label='Facebook'><Facebook size={18} /></a>
-            <a href='#' aria-label='Instagram'><Instagram size={18} /></a>
-            <a href='#' aria-label='LinkedIn'><Linkedin size={18} /></a>
+          <div className='socials' aria-label='Social media links'>
+            <a href='https://facebook.com' target='_blank' rel='noreferrer' aria-label='Facebook'>
+              <Facebook size={18} />
+            </a>
+            <a href='https://instagram.com' target='_blank' rel='noreferrer' aria-label='Instagram'>
+              <Instagram size={18} />
+            </a>
+            <a href='https://twitter.com' target='_blank' rel='noreferrer' aria-label='Twitter'>
+              <Twitter size={18} />
+            </a>
+            <a href='https://youtube.com' target='_blank' rel='noreferrer' aria-label='YouTube'>
+              <Youtube size={18} />
+            </a>
           </div>
         </div>
-      </div>
-
-      <div className='footer-bottom'>
-        © 2026 Amigo Rentals. All rights reserved.
       </div>
     </footer>
   );
